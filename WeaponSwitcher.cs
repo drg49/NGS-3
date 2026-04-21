91c8869def71a5b7059752da102cd10ab8d8531b
Danny Gavin <dan.gavin49@gmail.com>
2026-04-21 00:14:03 -0700

    Add camera movement

--- WeaponSwitcher.cs
+++ WeaponSwitcher.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using UnityEngine.InputSystem; // Add this line at the top!
+using UnityEngine.InputSystem;
+using Unity.Cinemachine;
 
 public class WeaponSwitcher : MonoBehaviour
 {
@@ -8,34 +9,41 @@ public class WeaponSwitcher : MonoBehaviour
     public RuntimeAnimatorController unarmedController;
     public AnimatorOverrideController pistolOverride;
 
-    [Header("Weapon Objects")]
+    [Header("Camera Configuration")]
+    public CinemachineVirtualCamera followCamera;
+    private Cinemachine3rdPersonFollow _thirdPersonFollow;
+
+    [Header("Settings")]
+    [Tooltip("Negative moves player right. Try -1.0 or -1.5 for a strong offset.")]
+    public float pistolCameraOffset = -1.0f;
     public GameObject pistolModel;
+
+    void Start()
+    {
+        if (followCamera != null)
+            _thirdPersonFollow = followCamera.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
+    }
 
     void Update()
     {
-        // New Input System way of checking keys
-        if (Keyboard.current.digit1Key.wasPressedThisFrame)
-        {
-            EquipUnarmed();
-        }
-
-        if (Keyboard.current.digit2Key.wasPressedThisFrame)
-        {
-            EquipPistol();
-        }
+        if (Keyboard.current.digit1Key.wasPressedThisFrame) EquipUnarmed();
+        if (Keyboard.current.digit2Key.wasPressedThisFrame) EquipPistol();
     }
 
     void EquipUnarmed()
     {
         animator.runtimeAnimatorController = unarmedController;
         if (pistolModel != null) pistolModel.SetActive(false);
-        Debug.Log("Switched to Unarmed");
+        if (_thirdPersonFollow != null) _thirdPersonFollow.ShoulderOffset.x = 0f;
     }
 
     void EquipPistol()
     {
         animator.runtimeAnimatorController = pistolOverride;
         if (pistolModel != null) pistolModel.SetActive(true);
-        Debug.Log("Switched to Pistol");
+
+        // Push the player significantly to the right of the screen
+        if (_thirdPersonFollow != null)
+            _thirdPersonFollow.ShoulderOffset.x = pistolCameraOffset;
     }
 }
